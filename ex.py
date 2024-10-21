03e394e6e29ce9cf175c9382297ba5985b1ef2a0
Wasif-Ansari <110668035+Wasif-Ansari@users.noreply.github.com>
2024-10-21 11:42:27 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,15 +1,58 @@
+# import streamlit as st
+# import pandas as pd
+
+# # Sample data
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+
+# # Create a DataFrame
+# ansar_data = pd.DataFrame(data)
+
+# # Display the DataFrame as a table
+# st.table(ansar_data)
+
+
+
+
 import streamlit as st
 import pandas as pd
+import openpyxl
 
-# Sample data
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
-}
+# Load the Excel file
+file_path = 'NAMAZ TIMINGS.xlsx'
+# excel_data = pd.read_excel(file_path)
 
-# Create a DataFrame
-ansar_data = pd.DataFrame(data)
+# st.table(pd.DataFrame({'Prayer': [ansar_prayer_name], 'Time': [ansar_time]}))
 
-# Display the DataFrame as a table
-st.table(ansar_data)
+
+st.title("Namaz Timings")
+
+st.subheader("NOORANI Timings")
+
+noorani_prayer_names = ['FAJAR', 'ZOHAR', 'ASAR', 'MAGHRIB', 'ISHA']
+
+wb = openpyxl.load_workbook(file_path)
+sheet = wb['Sheet1']
+
+C1 = sheet['C2'] # read direct value in cell C1
+noorani_time = [str(sheet[f"B{i}"].value)[:-3] for i in range(2,7)]
+
+st.table(pd.DataFrame({'Prayer': noorani_prayer_names, 'Time': noorani_time}))
+
+
+
+st.subheader("ANSAR Timings")
+
+ansar_time = [str(sheet[f"D{i}"].value)[:-3] for i in range(2,7)]
+
+st.table(pd.DataFrame({'Prayer': noorani_prayer_names, 'Time': ansar_time}))
+
+
+st.subheader("MARKAZ Timings")
+
+markaz_time = [str(sheet[f"F{i}"].value)[:-3] for i in range(2,7)]
+
+st.table(pd.DataFrame({'Prayer': noorani_prayer_names, 'Time': markaz_time} ))
